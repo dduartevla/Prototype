bce918f8a64eadc80b9fdd92ed2fa0e189ba0d4e
dduartevla <deboraduarte@ice.ufjf.br>
2023-09-25 17:54:32 -0300

    Classe Bancada e Builder ainda sem padrão

--- src/main/java/Bancada.java
+++ src/main/java/Bancada.java
@@ -1,2 +1,63 @@
+import java.util.Date;
+
 public class Bancada {
+
+    private int id;
+    private String nomeCultura;
+    private Date dataPlantio;
+    private Date dataPrevistaColheita;
+    private int capacidadeReservatorio;
+    private int maxUnidadesPlantio;
+
+    public Bancada(){
+        this.nomeCultura = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeCultura() {
+        return nomeCultura;
+    }
+
+    public void setNomeCultura(String nomeCultura) {
+        this.nomeCultura = nomeCultura;
+    }
+
+    public Date getDataPlantio() {
+        return dataPlantio;
+    }
+
+    public void setDataPlantio(Date dataPlantio) {
+        this.dataPlantio = dataPlantio;
+    }
+
+    public Date getDataPrevistaColheita() {
+        return dataPrevistaColheita;
+    }
+
+    public void setDataPrevistaColheita(Date dataPrevistaColheita) {
+        this.dataPrevistaColheita = dataPrevistaColheita;
+    }
+
+    public int getCapacidadeReservatorio() {
+        return capacidadeReservatorio;
+    }
+
+    public void setCapacidadeReservatorio(int capacidadeReservatorio) {
+        this.capacidadeReservatorio = capacidadeReservatorio;
+    }
+
+    public int getMaxUnidadesPlantio() {
+        return maxUnidadesPlantio;
+    }
+
+    public void setMaxUnidadesPlantio(int maxUnidadesPlantio) {
+        this.maxUnidadesPlantio = maxUnidadesPlantio;
+    }
 }
--- src/main/java/BancadaBuilder.java
+++ src/main/java/BancadaBuilder.java
@@ -1,2 +1,47 @@
+import java.util.Date;
+
 public class BancadaBuilder {
+
+    private Bancada bancada;
+
+    public BancadaBuilder(){
+        bancada = new Bancada();
+    }
+
+    public Bancada build(){
+        if (bancada.getNomeCultura().equals("")){
+            throw new IllegalArgumentException("Nome da Cultura inválido!");
+        }
+        return bancada;
+    }
+
+    public BancadaBuilder setId(int id) {
+        bancada.setId(id);
+        return this;
+    }
+
+    public BancadaBuilder setNomeCultura(String nomeCultura) {
+        bancada.setNomeCultura(nomeCultura);
+        return this;
+    }
+
+    public BancadaBuilder setDataPlantio(Date dataPlantio) {
+        bancada.setDataPlantio(dataPlantio);
+        return this;
+    }
+
+    public BancadaBuilder setDataPrevistaColheita(Date dataPrevistaColheita) {
+        bancada.setDataPrevistaColheita(dataPrevistaColheita);
+        return this;
+    }
+
+    public BancadaBuilder setCapacidadeReservatorio(int capacidadeReservatorio) {
+        bancada.setCapacidadeReservatorio(capacidadeReservatorio);
+        return this;
+    }
+
+    public BancadaBuilder setMaxUnidadesPlantio(int maxUnidadesPlantio) {
+        bancada.setMaxUnidadesPlantio(maxUnidadesPlantio);
+        return this;
+    }
 }
